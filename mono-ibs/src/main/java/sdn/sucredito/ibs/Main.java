ea4f07a611791c0de00069e2673cc574bd872cbe
esimionato <mail@mail.com>
2023-01-24 20:42:14 -0300

    U

--- mono-ibs/src/main/java/sdn/sucredito/ibs/Main.java
+++
@@ -1,7 +0,0 @@
-package sdn.sucredito.ibs;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ mono-ibs/src/main/java/sdn/sucredito/wincoin/ibs/windcoin/client/Main.java
@@ -0,0 +1,7 @@
+package sdn.sucredito.wincoin.ibs.windcoin.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[ sucredito :: wincoin IBS_CLIENT ]");
+    }
+}
